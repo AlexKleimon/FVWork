17313a442fe6a280fff339f4c9b0b6c2d6c69117
Alex <Utin97@mail.ru>
2023-01-28 15:44:58 +0300

    Added method OutputStringArray in file MyMethods.cs and verification methods

--- MyMethods.cs
+++ MyMethods.cs
@@ -6,9 +6,20 @@ public class MyMethods
         int lengthArray = dataArray.Length;
         for (int i = 0; i < lengthArray; i++)
         {
-            Console.WriteLine($"Введите элемент массива под номером {i}: ");
+            Console.WriteLine($"Введите элемент массива под индексом {i}: ");
             dataArray[i] = Console.ReadLine();
         }
         return dataArray;
+    }
+    public static void OutputStringArray(string[] dataArray)
+    {
+        int lengthArray = dataArray.Length;
+        string text = string.Empty;
+        Console.Write($"[ {dataArray[0]}");
+        for (int i = 0; i < lengthArray; i++)
+        {
+            text += $", {dataArray[i]}";
+        }
+        Console.Write($"{text} ]\n");
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿Console.Write("Введите количество элементов массива: ");
+int quntity = int.Parse(Console.ReadLine());
+string[] dataArray = MyMethods.InputStringArray(quntity);
+Console.Write("Массив: ");
+MyMethods.OutputStringArray(dataArray);
